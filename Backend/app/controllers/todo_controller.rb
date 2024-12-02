384e5dc46ddafef0db0e19adcb22b18db3e3bd00
Adelino Penedo <agpene@hotmail.com>
2024-12-02 10:26:05 -0300

    feat: filtering todos

--- Backend/app/controllers/todo_controller.rb
+++ Backend/app/controllers/todo_controller.rb
@@ -4,7 +4,9 @@ class TodoController < ApplicationController
 
   def index
     if current_user
-      render json: current_user.projects.includes(:todos).map(&:todos).flatten
+      todos = Todo.joins(:project).where(projects: { user_id: current_user.id })
+      todos = apply_filters(todos, params[:filters]) if params[:filters]
+      render json: todos
     else
       render json: { error: 'User not logged in' }, status: :unauthorized
     end
@@ -12,7 +14,9 @@ class TodoController < ApplicationController
 
   def project_todos
     project = current_user.projects.find(params[:project_id])
-    render json: project.todos
+    todos = project.todos
+    todos = apply_filters(todos, params[:filters]) if params[:filters]
+    render json: todos
   rescue ActiveRecord::RecordNotFound
     render json: { error: 'Project not found' }, status: :not_found
   end
@@ -55,18 +59,28 @@ class TodoController < ApplicationController
   end
 
   private
+      def set_todo
+        @todo = Todo.find(params[:id])
+      end
 
-  def set_todo
-    @todo = Todo.find(params[:id])
-  end
+      def todo_params
+        params.permit(:title, :description, :project_id)
+      end
 
-  def todo_params
-    params.permit(:title, :description, :project_id)
-  end
+      def authorize_user
+        unless @todo.project.user == current_user
+          render json: { error: 'Not Authorized' }, status: :unauthorized
+        end
+      end
 
-  def authorize_user
-    unless @todo.project.user == current_user
-      render json: { error: 'Not Authorized' }, status: :unauthorized
-    end
-  end
+      def apply_filters(todos, filters)
+        filters.each do |key, value|
+          if todos.column_names.include?(key.to_s)
+            todos = todos.where(key => value)
+          else
+            raise ArgumentError, "Invalid filter key: #{key}"
+          end
+        end
+        todos
+      end
 end
