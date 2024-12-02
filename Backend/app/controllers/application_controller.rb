400210742f6fb66c544b4da809276cd55e52509d
Adelino Penedo <agpene@hotmail.com>
2024-12-02 08:04:29 -0300

    Feat: Basic User authentication,registration and login

--- Backend/app/controllers/application_controller.rb
+++ Backend/app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  skip_before_action :verify_authenticity_token
+
+  include ActionController::Cookies
+
+  private
+
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id])
+  end
+
+  private
+
 end
