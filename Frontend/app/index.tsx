441c57331728dd739bb78673216b7ed4d7381cc9
Adelino Penedo <agpene@hotmail.com>
2024-12-03 22:41:49 -0300

    Feat: Custom description on initial page

--- Frontend/app/index.tsx
+++ Frontend/app/index.tsx
@@ -1,37 +1,31 @@
-import { StyleSheet } from "react-native";
 import { Text, View } from "@/components/Themed";
 import { TouchableOpacity } from "react-native";
-import { router } from "expo-router";
+import { router, useNavigation } from "expo-router";
+import tw from "twrnc";
 
-export default function TabOneScreen() {
+export default function Index() {
+  const navigation = useNavigation();
+  navigation.setOptions({ title: "" });
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>TodoPath</Text>
+    <View style={tw`flex-1 items-center justify-center`}>
+      <Text style={tw`text-xl font-bold`}>TodoPath</Text>
+
       <View
-        style={styles.separator}
+        style={tw`my-8 h-px w-11/12 max-w-md`}
         lightColor="#eee"
         darkColor="rgba(255,255,255,0.1)"
       />
+
+      <Text style={tw`text-center text-base mx-4 my-4 bg-white p-6 rounded-lg shadow-md w-11/12 max-w-md`}>
+        This project is a Todo App built with Ruby on Rails for the backend and
+        React Native for the frontend. It allows users to create, read, update,
+        and delete tasks. Tasks are organized into projects, enabling users to
+        manage their tasks more efficiently by grouping related tasks together.
+      </Text>
+
       <TouchableOpacity onPress={() => router.navigate("/login")}>
-        <Text style={{ color: "blue" }}>Go to Login</Text>
+        <Text style={tw`text-blue-500`}>Go to Login</Text>
       </TouchableOpacity>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
-});
