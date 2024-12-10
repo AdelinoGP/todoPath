9e4fa07bc074d8b660017817e7f84b147e1507be
Adelino Penedo <agpene@hotmail.com>
2024-12-10 19:09:49 -0300

    Logging errors while running tests

--- Backend/config/environments/test.rb
+++ Backend/config/environments/test.rb
@@ -50,4 +50,10 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # Enable stdout logger
+  config.logger = Logger.new(STDOUT)
+
+  # Set log level
+  config.log_level = :error
 end
